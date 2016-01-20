86f46701ba3e66c7ebd8c9eeb984c820cbeb87b0
gjkoni <gj_koni@hotmail.com>
2016-01-20 17:09:50 +0800

    example poll project test

---
+++ app.js
@@ -0,0 +1,45 @@
+/*jshint node:true*/
+var express = require('express');
+var favicon = require('static-favicon');
+var session = require('express-session'); 
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan')
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.set('port', process.env.VCAP_APP_PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon());
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Errors gracefully
+app.use(function(err, req, res, next) {
+	if(!err) return next();
+	console.log(err.stack);
+	res.json({error: true});
+});
+
+// Main App Page
+app.get('/', routes.index);
+
+// MongoDB API Routes
+app.get('/polls/polls', routes.list);
+app.get('/polls/:id', routes.poll);
+app.post('/polls', routes.create);
+app.post('/vote', routes.vote);
+
+io.sockets.on('connection', routes.vote);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
